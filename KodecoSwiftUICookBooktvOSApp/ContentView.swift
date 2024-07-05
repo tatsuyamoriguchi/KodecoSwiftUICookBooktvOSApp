c42bc0e8b115b8762738c523f7a2a6b6e4342224
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-07-05 08:51:29 -0700

    tvOS app

--- KodecoSwiftUICookBooktvOSApp/ContentView.swift
+++ KodecoSwiftUICookBooktvOSApp/ContentView.swift
@@ -8,12 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    let items = ["item1","item2", "item3"]
+    @State private var selection: Int? = nil
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("My List")
+            
+            ForEach(0..<items.count, id: \.self) { index in
+                Button(action: {
+                    self.selection = index
+                }) {
+                    HStack {
+                        Text(self.items[index])
+                        Spacer()
+                        if selection == index {
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+                .padding()
+            }
+            if let selection = selection {
+                Text("Selected Item: \(items[selection])")
+            }
         }
         .padding()
     }
